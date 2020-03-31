7773d5b97ab07006a170683e34636b1c234bdd53
Fernando Alves <fernando.alves@jussi.com.br>
2020-03-30 21:41:23 -0300

    Add component HeroCarrousel.

--- react/Index.js
+++ react/Index.js
@@ -1,22 +1,21 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { SEO } from 'brastemp.components';
 import { ExtensionPoint } from "render";
 import LavaLoucas from "./LavaLoucas/LavaLoucas";
 import LazyLoad from 'react-lazyload';
 
 class lavaloucas extends Component {
- 
-    render () { 
-        return(
+
+    render() {
+        return (
             <React.Fragment>
                 <LazyLoad height={'100%'} offset={0}>
                     <SEO title={"Brastemp | Sem dúvida, Brastemp."} description={"Eletrodomésticos e acessórios para sua cozinha e lavanderia. Conheça nossa linha de fogões, fornos, geladeiras, lavadoras, microondas e muito mais."} />
                     <main id="main" className="main">
-                    <ExtensionPoint id="header" hideSupportMenu={true} />
-                    {/* <ExtensionPoint id="HeroCarousel" /> */}
-                	<LavaLoucas />
-                    <ExtensionPoint id="footer"  hideSupportMenu={true} />
-                </main>
+                        <ExtensionPoint id="header" hideSupportMenu={true} />
+                        <LavaLoucas />
+                        <ExtensionPoint id="footer" hideSupportMenu={true} />
+                    </main>
                 </LazyLoad>
             </React.Fragment>
         )
